2d2a3f913693cee92b83dd21932935942e082f3b
Annmarie SanSevero <sophia.kai.charis@gmail.com>
2024-07-17 17:31:26 -0500

    updated routes

--- scriptureApp/src/app/app.routes.ts
+++ scriptureApp/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { ScriptureListComponent } from './scriptures-list/scriptures-list.component';
-
+import { AddScriptureComponent } from './add-scripture/add-scripture.component';
+import { EditScriptureComponent } from './edit-scripture/edit-scripture.component';
 
 export const routes: Routes = [
-    { path: '', component: ScriptureListComponent, title: 'Scriptures List'}
+    { path: '', component: ScriptureListComponent, title: 'Scriptures List'}, 
+    { path: 'new', component: AddScriptureComponent },
+    { path: 'edit/:id', component: EditScriptureComponent }
 ];
